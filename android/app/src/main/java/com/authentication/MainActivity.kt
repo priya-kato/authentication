26fca019d7fa2fa5bcb6da9e5c1203eae8b97fef
Priya <priya.r@katomaran.tech>
2024-02-28 10:29:19 +0530

    text hide

--- android/app/src/main/java/com/authentication/MainActivity.kt
+++ android/app/src/main/java/com/authentication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.authentication
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -18,5 +20,6 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
+  SplashScreen.show(this);
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
